b015e449e239ec14066ff91aa381c5b286f7736e
Shreesha Rao <shreesha.rao@ihsmarkit.com>
2018-08-14 15:21:30 +0530

    initial commit

---
+++ src/SSR.PL.Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SSR.PL.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
