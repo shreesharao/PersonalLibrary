4097dd62fb9b498599af2fc636401bbfc5db96cd
shreesharao <sshreesha4@gmail.com>
2018-09-28 13:53:39 +0530

    Resource based authorization covered

--- src/SSR.PL.Web/Controllers/LibraryController.cs
+++ src/SSR.PL.Web/Controllers/LibraryController.cs
@@ -6,12 +6,22 @@ namespace SSR.PL.Web.Controllers
 {
     public class LibraryController : Controller
     {
+        IAuthorizationService _authorizationService = null;
+        public LibraryController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
         [HttpGet]
-        [RoleAuthorize("Administrator",AuthenticationSchemes = "Identity.Application,Identity.External")]
+        [RoleAuthorize("Administrator", AuthenticationSchemes = "Identity.Application,Identity.External")]
         [Authorize(AuthenticationSchemes = "Identity.Application,Identity.External", Policy = "AdministratorUsers")]
         public IActionResult Dashboard()
         {
-            return View();
+            //RoleAdministrator is the policy name constructed in RoleAuthorizeAttribute class
+            if (_authorizationService.AuthorizeAsync(User, "RoleAdministrator").Result.Succeeded)
+            {
+                return View();
+            }
+            return RedirectToAction("Login", "Profile");
         }
     }
 }
